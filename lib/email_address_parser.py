4b950f4ad351e677fc67d3cf9dd7d02f58465692
Brian Kaloki <brianngenye@gmail.com>
2025-05-14 11:17:33 +0300

    my first commit

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,18 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        # Split input on comma or whitespace
+        parts = re.split(r'[,\s]+', self.addresses.strip())
+
+        # Define a basic email regex
+        email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w{2,}$')
+
+        # Filter only valid emails and remove duplicates
+        unique_emails = {email for email in parts if email_pattern.fullmatch(email)}
+
+        # Return sorted list
+        return sorted(unique_emails)
